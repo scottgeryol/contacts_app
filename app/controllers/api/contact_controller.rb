715712298788d6392bdbb32cb1d4c3f513addc66
Scott Geryol <sgeryol@gmail.com>
2020-03-08 10:29:51 -0500

    restful contacts app

--- app/controllers/api/contact_controller.rb
+++ app/controllers/api/contact_controller.rb
@@ -1,11 +1,38 @@
 class Api::ContactController < ApplicationController
-  def one_contact_method
-    @contact = Contact.first
-    render "one_contact.json.jb"
+  def index
+    @contacts = Contact.all
+    render "index.json.jb"
   end
 
-  def all_contact_method
-    @contact = Contact.all
-    render "all_contact.json.jb"
+  def create
+    @contact = Contact.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:emai],
+    )
+    @contact.save
+    render "show.json.jb"
+  end
+
+  def show
+    @contact = Contact.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @contact = Contact.find_by(id: params[:id])
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.phone_number = params[:phone_number]
+    @contact.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    contact = Contact.find_by(id: params[:id])
+    contact.destroy
+    render json: { message: "You have destroyed this person!" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
-    get "/contact_1_url" => "contact#one_contact_method"
-    get "/contact_all_url" => "contact#all_contact_method"
+    get "/contacts" => "contact#index"
+    post "/contacts" => "contact#create"
+    get "/contacts/:id" => "contact#show"
+    patch "/contacts/:id" => "contact#update"
+    delete "/contacts/:id" => "contact#destroy"
   end
 end
