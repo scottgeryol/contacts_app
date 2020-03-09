d4e45180dde144ddbe889526afb99176cf22ef36
Scott Geryol <sgeryol@gmail.com>
2020-03-09 18:58:32 -0500

    Adding Changes in Database

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,10 @@
 class Contact < ApplicationRecord
+  def friendly_updated_at
+    updated_at.strftime("%B %e, %Y")
+  end
+
+  def full_name
+    full_name = "#{first_name} #{last_name}"
+    full_name
+  end
 end
